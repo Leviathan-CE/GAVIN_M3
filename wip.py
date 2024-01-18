084b22b34b4cbe5636498a418c4246e39c120139
Leviathan <leviathancreative@outlook.com>
2024-01-18 01:39:50 -0800

    started content

--- wip.py
+++ wip.py
@@ -4,11 +4,33 @@ from PyQt6.QtWidgets import (
     QApplication
 
 )
+
 from src.data.paths import GUI_STYLES
 if __name__ == "__main__":
-    app = QApplication([])
-    btn = form()
-    btn.show()
+    markdown_content = """
+    This is an inline math example $e^{i\\pi} + 1 = 0$ 
+
+    # hi lo
+
+    This is a code block with syntax highlighting:
+
+    ```Java
+
+    public void hello_world(){
+        print("Hello, world!");
+        }
+
+    hello_world();
+    ```
+    And this is a displayed math example:
+
+    $$\\sum_{i=1}^{n} i = \\frac{n(n+1)}{2}$$ 
+
+    """
+    import sys
+    app = QApplication(sys.argv)
+    btn = form()   
+    
     file = open(GUI_STYLES+"\\dark_mode.css","r")  
     app.setStyleSheet(file.read())
     app.exec()
